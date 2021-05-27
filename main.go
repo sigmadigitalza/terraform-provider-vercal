5e426c11f845327e3d0c75e3cc668ef785e4599b
Shaun Egan <shauneganza@gmail.com>
2021-05-27 12:58:28 +0200

    fix(go-modules): Correct module name

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"letmein.to/terraform-provider-vercel/vercel"
+	"github.com/sigmadigitalza/terraform-provider-vercel/vercel"
 )
 
 func main() {
